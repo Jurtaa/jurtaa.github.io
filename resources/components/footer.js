5f222498b144eec71bb78044effc39feb5e0e989
Jurta <29545237+Jurtaa@users.noreply.github.com>
2022-05-29 18:50:25 +0100

    LOL

--- resources/components/footer.js
+++ resources/components/footer.js
@@ -3,18 +3,21 @@ const footer = document.createElement('template');
 footer.innerHTML = `
 	<div class="footer">
 		<div class="footer-content">
-			<div class="textdrop">
-				<div style="float: left;">
-					<p>© Jurta 2022-2022</p>
+			<a href="https://youtube.com/Jurta" target="_blank" rel="noopener noreferrer">
+				<div class="social-button">
+					<img src="resources/img/socials/youtube.svg"></img>
 				</div>
-				<div style="float: right;">
-					<p>
-						<a href="https://youtube.com/Jurta">youtube</a>
-						<a href="https://twitter.com/Jurta_">twitter</a>
-						<a href="https://github.com/Jurtaa">github</a>
-					</p>
+			</a>
+			<a href="https://twitter.com/Jurta_" target="_blank" rel="noopener noreferrer">
+				<div class="social-button">
+					<img src="resources/img/socials/twitter.svg"></img>
 				</div>
-			</div>
+			</a>
+			<a href="https://github.com/Jurtaa" target="_blank" rel="noopener noreferrer">
+				<div class="social-button">
+					<img src="resources/img/socials/github.svg"></img>
+				</div>
+			</a>
 		</div>
 	</div>
 `;
