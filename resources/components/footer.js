9b8fd5ff8a8be88600215744d56487ee635e759d
Jurta <29545237+Jurtaa@users.noreply.github.com>
2022-06-01 11:55:33 +0100

    happy pride month :)

--- resources/components/footer.js
+++ resources/components/footer.js
@@ -37,8 +37,11 @@ function registerSeperator() {
 	document.getElementById("footer-content").appendChild(seperate);
 }
 
+// miscellaneous (mostly discord embed tricks)
+registerButton("/pride.mpeg4", "HAPPY PRIDE MONTH 2022 >:D", "/resources/img/icons/pride_month_2022.svg");
 registerButton("/autism.gif", "discord embed trick thing :)", "/resources/img/autism.gif");
 registerSeperator();
+// social media links
 registerButton("https://youtube.com/Jurta", "YouTube", "/resources/img/socials/youtube.svg");
 registerButton("https://twitter.com/Jurta_", "Twitter", "/resources/img/socials/twitter.svg");
 registerButton("https://github.com/Jurtaa", "GitHub", "/resources/img/socials/github.svg");
